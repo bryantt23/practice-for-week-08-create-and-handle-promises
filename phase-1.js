bfde3bab052f4ff906805ab9231731b8d60ca8eb
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-08 02:37:28 -0700

    do with async await

--- phase-1.js
+++ phase-1.js
@@ -1,17 +1,38 @@
 function stretch() {
   // Your code here
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+      console.log('done stretching');
+    }, 1000);
+  });
 }
 
 function runOnTreadmill() {
   // Your code here
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('done running on treadmill');
+      resolve();
+    }, 500);
+  });
 }
 
 function liftWeights() {
   // Your code here
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('done lifting weights');
+      resolve();
+    }, 2000);
+  });
 }
 
-function workout() {
+async function workout() {
   // Your code here
+  await stretch();
+  await runOnTreadmill();
+  await liftWeights();
 }
 
 /* ============================ TEST YOUR CODE ============================
