bd82f2e3382b8f0b6e13c1e0da7eb611e3601756
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-08 02:25:49 -0700

    initial

--- phase-1.js
+++ phase-1.js
@@ -14,17 +14,15 @@ function workout() {
   // Your code here
 }
 
-
 /* ============================ TEST YOUR CODE ============================
 
 Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
 
-
 workout();
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // done lifting weights
-    // done working out
+// should print out the following:
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
